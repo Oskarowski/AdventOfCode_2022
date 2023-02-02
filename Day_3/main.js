c636212acf2fb218937e53967bd320f5f6a3983c
Oskarowski <oscar.kacprzak@gmail.com>
2023-02-02 20:01:33 +0100

    added key: letter value: priority

--- Day_3/main.js
+++ Day_3/main.js
@@ -14,21 +14,37 @@ individualRucksacks.forEach((rucksack) => {
   rucksackCompartments.push(rucksack.slice(rucksackLength / 2));
   rucksacksCompartments.push(rucksackCompartments);
 });
-console.log(rucksacksCompartments);
+// console.log(rucksacksCompartments);
 
 let appearsInBothCompartments = [];
 rucksacksCompartments.forEach((rucksack) => {
   let indiCompartment = rucksack[0];
-  console.log(indiCompartment);
+  // console.log(indiCompartment);
   for (let i = 0; i < rucksack[0].length; i++) {
     let letter = rucksack[0][i];
-    console.log(letter);
+    // console.log(letter);
     if (rucksack[1].includes(letter)) {
       appearsInBothCompartments.push(letter);
-      console.log("appearsInBothCompartments: " + letter);
+      // console.log("appearsInBothCompartments: " + letter);
       break;
     }
   }
 });
 
 console.log(appearsInBothCompartments);
+
+const alphaLower = Array.from(Array(26)).map((e, i) => i + 97);
+const alphaUpper = Array.from(Array(26)).map((e, i) => i + 65);
+
+const alphabetLower = alphaLower.map((e) => String.fromCharCode(e));
+const alphabetUpper = alphaUpper.map((e) => String.fromCharCode(e));
+
+const fullAlphaber = [].concat(alphabetLower, alphabetUpper);
+
+let priority = 1;
+let lettersWithPriority = [];
+fullAlphaber.forEach((letter) => {
+  lettersWithPriority[letter] = priority;
+  priority++;
+});
+console.log(lettersWithPriority);
