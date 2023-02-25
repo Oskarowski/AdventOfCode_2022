8e513723f36de0a9f76550867b226089e61509a3
Oskarowski <31699795+Oskarowski@users.noreply.github.com>
2023-02-25 12:01:31 +0100

    -fixed typo in variable name in PART 1 -done PART 2, with correct answer for input data

--- Day_6/solution.js
+++ Day_6/solution.js
@@ -5,7 +5,7 @@ const splitInput = fs
   .toString()
   .split("\r\n");
 
-var markerAppears = [];
+const packetMarkerAppears = [];
 
 splitInput.forEach((line) => {
   var markerFound = false;
@@ -15,9 +15,9 @@ splitInput.forEach((line) => {
     }
     const inLineLettes = line.substring(i, i + 4);
     markerFound = true;
-    for (var j = 0; j < inLineLettes.length - 1; j++) {
-      const regex = inLineLettes[j];
-      const regexObj = new RegExp(regex, "g");
+    for (var j = 0; j < inLineLettes.length; j++) {
+      const regexp = inLineLettes[j];
+      const regexObj = new RegExp(regexp, "g");
       const lettersMatch = inLineLettes.match(regexObj);
       if (lettersMatch.length > 1) {
         markerFound = false;
@@ -25,10 +25,39 @@ splitInput.forEach((line) => {
       }
     }
     if (markerFound) {
-      markerAppears.push(i + 4);
+      packetMarkerAppears.push(i + 4);
     }
   }
 });
 console.log(
-  `The first start-of-packet marker is detected after: ${markerAppears} characters`
+  `The first start-of-packet marker is detected after: ${packetMarkerAppears} characters`
+);
+// PART 2
+const messageMarkerAppears = [];
+splitInput.forEach((line) => {
+  var messageMarkerFound = false;
+  for (var i = 0; i < line.length - 14; i++) {
+    if (messageMarkerFound) {
+      break;
+    }
+    const inLineLettes = line.substring(i, i + 14);
+    for (var j = 0; j < inLineLettes.length; j++) {
+      const regexp = inLineLettes[j];
+      messageMarkerFound = true;
+      const regexObj = new RegExp(regexp, "g");
+
+      const lettersMatch = inLineLettes.match(regexObj);
+
+      if (lettersMatch.length > 1) {
+        messageMarkerFound = false;
+        break;
+      }
+    }
+    if (messageMarkerFound) {
+      messageMarkerAppears.push(i + 14);
+    }
+  }
+});
+console.log(
+  `The first start-of-message marker is detected after: ${messageMarkerAppears} characters`
 );
