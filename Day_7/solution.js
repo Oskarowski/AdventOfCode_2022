752d490f9e27fc14a49f507cbf15b71843571ff4
Oskarowski <31699795+Oskarowski@users.noreply.github.com>
2023-02-26 21:07:29 +0100

    slicing and triming

--- Day_7/solution.js
+++ Day_7/solution.js
@@ -3,4 +3,21 @@ const puzzleInputSplit = fs
   .readFileSync(`${__dirname}/input`)
   .toString()
   .split("$");
-console.log(puzzleInputSplit);
+puzzleInputSplit.shift();
+puzzleInputSplit.shift();
+
+tree = { "/": null };
+
+var direcotoryRightNow = "/";
+
+console.log("\n");
+
+puzzleInputSplit.forEach((commendLine, whichLine) => {
+  commendLine = commendLine.trimStart();
+  commendLine = commendLine.replace(/\s{1,}/g, " ");
+  console.log("commendLine:", commendLine);
+  var commend = commendLine.slice(0, 2);
+  commendLine = commendLine.slice(3);
+  console.log("commend:", commend);
+  console.log("commendLine:", commendLine);
+});
