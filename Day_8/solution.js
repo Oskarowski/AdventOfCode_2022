164196ac95abea4fa18dd716600ddf4b43dc5321
Oskarowski <31699795+Oskarowski@users.noreply.github.com>
2023-03-05 16:08:19 +0100

    working part 1 for test input

--- Day_8/solution.js
+++ Day_8/solution.js
@@ -6,15 +6,76 @@ const puzzleInput = fs
   .replace(/\r/g, "")
   .split("\n");
 
-// console.log("puzzleInput", puzzleInput);
+function prepareTheGrid() {
+  const gridBoard = [];
+  puzzleInput.forEach((line) => {
+    const lineGrid = [];
+    for (var i = 0; i < line.length; i++) {
+      lineGrid.push(parseInt(line[i]));
+    }
+    gridBoard.push(lineGrid);
+  });
+  return gridBoard;
+}
 
-const gridBoard = [];
-puzzleInput.forEach((line) => {
-  const lineGrid = [];
-  for (var i = 0; i < line.length - 1; i++) {
-    lineGrid.push(parseInt(line[i]));
+function addToAreVisable(lineY, columnX, areVisable) {
+  //   areVisable.push(`(${columnX},${lineY})`);
+  areVisable.push(parseInt(`${columnX}${lineY}`));
+}
+
+function checkLineInDirection(
+  lineY,
+  columnX = 0,
+  dy,
+  dx,
+  gridBoard,
+  areVisable
+) {
+  addToAreVisable(lineY, columnX, areVisable);
+  let theHighestTree = gridBoard[lineY][columnX];
+  while (true) {
+    lineY += dy;
+    columnX += dx;
+    if (
+      lineY < 0 ||
+      lineY >= gridBoard.length ||
+      columnX < 0 ||
+      columnX >= gridBoard[lineY].length
+    ) {
+      break;
+    }
+    if (gridBoard[lineY][columnX] > theHighestTree) {
+      addToAreVisable(lineY, columnX, areVisable);
+      theHighestTree = gridBoard[lineY][columnX];
+    }
   }
-  gridBoard.push(lineGrid);
-});
+}
 
-console.log("gridBoard", gridBoard);
+(function () {
+  const gridBoard = prepareTheGrid();
+  console.log("gridBoard", gridBoard);
+  const areVisable = [];
+
+  //   checkLineInDirection(4, 2, -1, 0, gridBoard, areVisable);
+
+  for (let c = 0; c < gridBoard[0].length; c++) {
+    checkLineInDirection(0, c, 1, 0, gridBoard, areVisable);
+    checkLineInDirection(gridBoard.length - 1, c, -1, 0, gridBoard, areVisable);
+  }
+
+  for (let r = 0; r < gridBoard.length; r++) {
+    checkLineInDirection(r, 0, 0, 1, gridBoard, areVisable);
+    checkLineInDirection(
+      r,
+      gridBoard[0].length - 1,
+      0,
+      -1,
+      gridBoard,
+      areVisable
+    );
+  }
+  const areVisableSet = new Set(areVisable);
+  console.log(
+    `${areVisableSet.size} trees are visible from outside the grid\n`
+  );
+})();
