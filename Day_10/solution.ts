78feb7674ee3282cb19ddacd76202d6dc9054bbc
Oskarowski <31699795+Oskarowski@users.noreply.github.com>
2023-10-05 20:56:22 +0200

    task 2 done

--- Day_10/solution.ts
+++ Day_10/solution.ts
@@ -7,6 +7,7 @@ const puzzleInput = fs
   .split("\n");
 
 function task1(file: String) {
+  console.time("Task 1");
   let signalStrength: number = 0;
   let xRegister: number = 1;
   let cycleCount: number = 0;
@@ -30,10 +31,50 @@ function task1(file: String) {
       xRegister += parseInt(command.split(" ")[1]);
     }
   }
-
-  console.log("Task 1 || The sum of signal strengths: " + signalStrength);
+  console.timeLog("Task 1");
+  console.log("The sum of signal strengths: " + signalStrength + "\n");
 }
 
 const signalStrengths = task1(
   fs.readFileSync(`${__dirname}/input`).toString().replace(/\r/g, "")
 );
+
+function task2(file: String) {
+  console.time("Task 2");
+  const commands: Array<string> = file.split("\n");
+
+  let litPixels = [] as Array<number>;
+  let xRegister: number = 1;
+  let cycle: number = 0;
+
+  const tickCycle = () => {
+    cycle++;
+
+    if (xRegister <= cycle && cycle <= xRegister + 2) {
+      litPixels.push(cycle - 1);
+    }
+
+    if (cycle === 40) {
+      for (let xId = 0; xId < 40; xId++) {
+        litPixels.includes(xId)
+          ? process.stdout.write("#")
+          : process.stdout.write(".");
+      }
+      console.log("");
+      litPixels = [];
+      cycle = 0;
+    }
+  };
+
+  for (const commend of commands) {
+    tickCycle();
+
+    if (commend.startsWith("addx")) {
+      tickCycle();
+      xRegister += parseInt(commend.split(" ")[1]);
+    }
+  }
+  console.timeLog("Task 2");
+}
+
+task2(fs.readFileSync(`${__dirname}/input`).toString().replace(/\r/g, ""));
